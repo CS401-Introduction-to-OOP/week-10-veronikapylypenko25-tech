83f6acee0448fa8e88329260d4c284820778971a
Veronika Pylypenko <veronika.pylypenko.25@kse.org.ua>
2026-04-07 19:00:29 +0300

    Part of Party is done

--- Week10_Pylypenko/Party.cs
+++ Week10_Pylypenko/Party.cs
@@ -1,6 +1,27 @@
-﻿namespace Week10_Pylypenko;
+﻿using System.Collections;
+using System.Collections.Generic;
+namespace Week10_Pylypenko;
 
-public class Party
+public class Party:IEnumerable<Character>
 {
-    
+  private readonly List<Character> _characters = new List<Character>();
+
+  public void Add(Character character)
+  {
+    _characters.Add(character);
+  }
+
+  public IEnumerator<Character> GetEnumerator()
+  {
+    foreach (var character in _characters)
+    {
+      yield return character;
+    }
+  }
+   IEnumerator IEnumerable.GetEnumerator()
+  {
+    return GetEnumerator();
+  }
+   
+  
 }
