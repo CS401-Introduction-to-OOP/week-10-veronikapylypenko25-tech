c7936e0c3e9ca17e711072bd034f78d39a0c2124
Veronika Pylypenko <veronika.pylypenko.25@kse.org.ua>
2026-04-07 18:31:44 +0300

    Part of class Character is completed

---
+++ Week10_Pylypenko/Character.cs
@@ -0,0 +1,46 @@
+﻿namespace Week10_Pylypenko;
+
+public enum CharacterStatus
+{
+    Active, 
+    Injured, 
+    Dead
+}
+
+public class Character
+{
+    public string Name { get; set; }
+    public string Role { get; set; }
+    public int Level { get; set; }
+    public int Welbeeing { get; set; }
+    public int Gold { get; set; }
+    public CharacterStatus Status { get; set; }
+
+    public Character(string name, string role, int level, int welbeeing, int gold, CharacterStatus status)
+    {
+        Name = name;
+        Role = role;
+        Level = level;
+        Welbeeing = welbeeing;
+        Gold = gold;
+        Status = status;
+    }
+    public void HpChange(int amount)
+    {
+        Welbeeing += amount;
+        if (Welbeeing <= 0)
+        {
+            Welbeeing = 0;
+            Status = CharacterStatus.Dead;
+        }
+        else if (Welbeeing <= 45)
+        {
+            Status = CharacterStatus.Injured;
+        }
+        else
+        {
+            Status = CharacterStatus.Active;
+        }
+    }
+    
+}
